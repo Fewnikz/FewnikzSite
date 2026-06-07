a8dd3dba71e3bffb14ec417da2bdd111092eb881
Fewnikz <fewnikz@localhost.localdomain>
2026-06-07 21:03:15 +0200

    Made a hidden easter egg

---
+++ src/javascript/indexScript.js
@@ -0,0 +1,5 @@
+function cantDenyIt() {
+    const textChange = "Can't deny the furry allegations at this point :3"
+    document.getElementById(":3").textContent = textChange
+    document.getElementById(":3").style = "opacity: 1;"
+}
