44fb3fec4169cde42270ac17a48ab54e6c217f6a
Fewnikz <Fewnikz@proton.me>
2026-06-20 10:01:48 +0200

    Cleaned up some stuff and made animations for buttons

--- src/javascript/indexScript.js
+++ src/javascript/indexScript.js
@@ -1,5 +1,5 @@
 function cantDenyIt() {
     const textChange = "Can't deny the furry allegations at this point :3"
     document.getElementById(":3").textContent = textChange
-    document.getElementById(":3").style = "opacity: 1;"
+    document.getElementById(":3").style = "opacity: 1; transition: opacity 1s;"
 }
